df55e81b78340d38f155a85d203d3ee2e2e4b28b
Radu Giuclea <radu-alexandru.giuclea@kainos.com>
2022-09-15 12:19:54 +0100

    Implemented user story 1 interface

--- govuk/app/routes.js
+++ govuk/app/routes.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const interface = require('./interface.js')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+
+router.post('/addemployee', async (req, res) => {
+    var newEmployee = req.body;
+    var insertedKey = await interface.addEmployee(newEmployee);
+    if(insertedKey == null)
+        console.log("Something went wrong");
+    else
+        console.log("Inserted successfully.")
+})
 
 module.exports = router
