280f2efc2e9ba1948b907fa37b1ffce7a0e15218
Abiye <mieabiye.selema@kainos.com>
2022-09-15 15:20:55 +0100

    GetRequest- for User Story 2

--- govuk/app/interface.js
+++ govuk/app/interface.js
@@ -1,1 +1,17 @@
-// New functions for the interface go here:
+const axios = require('axios');
+
+
+exports.getEmployees = async () => {
+    let Employees = []
+    try{
+        const viewEmployee = await axios.get('http://localhost:8080/api/report/hr')
+        console.log(viewEmployee.data)
+        return viewEmployee.data;
+
+    }
+    catch(e) {
+        console.log(e);
+
+    }
+    return("Could not return HR employees")
+}
--- govuk/app/routes.js
+++ govuk/app/routes.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const interface = require('./interface.js')
 const router = express.Router()
+const employee = require('./interface.js')
 
 // Add your routes here - above the module.exports line
+
+router.get('/view-hr-employee', async (req, res) => {
+    var result = await employee.getEmployees()
+    res.render('view-hr-employee', {
+    employees: result.getEmployeesNames
+    });
+})
 
 module.exports = router
