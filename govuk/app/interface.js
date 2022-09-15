67d62d76de1bd2c6dc744b87098d3d5e47e69998
Radu Giuclea <radu-alexandru.giuclea@kainos.com>
2022-09-15 16:04:27 +0100

    Implemented user story 3

--- govuk/app/interface.js
+++ govuk/app/interface.js
@@ -3,7 +3,19 @@ const axios = require('axios');
 
 exports.addEmployee = async (newEmployee) => {
     try {
-        var insertIndex = await axios.post('http://localhost:8080/api/enter/employee', newEmployee)
+        console.log(newEmployee)
+        var insertIndex = await axios.post('http://localhost:8080/api/enter/employee', newEmployee);
+        return insertIndex;
+    }
+    catch(e) {
+        console.log(e);
+        return null;
+    }
+}
+
+exports.addSalesEmployee = async (newSalesEmployee) => {
+    try {
+        var insertIndex = await axios.post('http://localhost:8080/api/enter/salesEmployee', newSalesEmployee);
         return insertIndex;
     }
     catch(e) {
--- govuk/app/routes.js
+++ govuk/app/routes.js
@@ -4,8 +4,25 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.post('/checkemployee', async (req, res) => {
+    if(req.session.data['department'] == "SALES")
+        res.redirect('new-sales');
+    else {
+        var newEmployee = req.body;
+
+            var insertedKey = await interface.addEmployee(newEmployee);
+            if(insertedKey == null)
+                console.log("Something went wrong");
+            else {
+                res.render("insert-success")
+                console.log("Inserted successfully.")
+            }
+    }
+});
+
 router.post('/addemployee', async (req, res) => {
     var newEmployee = req.body;
+
     var insertedKey = await interface.addEmployee(newEmployee);
     if(insertedKey == null)
         console.log("Something went wrong");
@@ -13,6 +30,17 @@ router.post('/addemployee', async (req, res) => {
         res.render("insert-success")
         console.log("Inserted successfully.")
     }
-})
+});
+
+router.post('/addsalesemployee', async(req, res) => {
+    var newSalesEmployee = req.body;
+    var insertedKey = await interface.addSalesEmployee(newSalesEmployee);
+    if(insertedKey == null)
+        console.log("Something went wrong.");
+    else {
+        res.render("insert-success")
+        console.log("Sales employee inserted successfully.")
+    }
+});
 
 module.exports = router
